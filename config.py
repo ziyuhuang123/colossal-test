31a60dea66c455ee5c00e12021729c21f8b52640
BigOneLiXiaoMing <99854690+BigOneLiXiaoMing@users.noreply.github.com>
2022-09-14 20:51:30 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,28 @@
 from colossalai.amp import AMP_TYPE
 
-BATCH_SIZE = 128
-NUM_EPOCHS = 200
+# BATCH_SIZE = 128
+# NUM_EPOCHS = 200
 
-fp16=dict(
-    mode = AMP_TYPE.NAIVE  
-)
+# fp16=dict(
+#     mode = AMP_TYPE.NAIVE  
+# )
+
+# parallel = dict(
+#     pipeline=2,
+#     tensor=dict(size=4, mode='2d')
+# )
+
+BATCH_SIZE = 512
+LEARNING_RATE = 2e-3
+WEIGHT_DECAY = 3e-2
+
+TENSOR_PARALLEL_SIZE = 8
+TENSOR_PARALLEL_MODE = '3d'
+
+NUM_EPOCHS = 200
+WARMUP_EPOCHS = 40
 
 parallel = dict(
-    pipeline=2,
-    tensor=dict(size=4, mode='2d')
+    pipeline=1,
+    tensor=dict(mode=TENSOR_PARALLEL_MODE, size=TENSOR_PARALLEL_SIZE),
 )
